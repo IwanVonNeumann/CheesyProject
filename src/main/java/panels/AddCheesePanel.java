92a01066bd26e4ff80367c9e8b56580bff0a507c
Iwan <grabageheap@inbox.lv>
2014-03-29 10:15:28 +0200

    Compound Property Model added to AddCheesePanel

--- src/main/java/panels/AddCheesePanel.java
+++ src/main/java/panels/AddCheesePanel.java
@@ -4,7 +4,7 @@ import domain.Cheese;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.CompoundPropertyModel;
 import views.ViewCheeses;
 
 public class AddCheesePanel extends CheesePanel {
@@ -12,35 +12,22 @@ public class AddCheesePanel extends CheesePanel {
     public AddCheesePanel(String id) {
         super(id);
 
-        final Cheese cheese = new Cheese();
+        Cheese cheese = new Cheese();
 
-        Form form = new Form("form") {
-            /*
-            @Override
-            protected void onSubmit() {
-                super.onSubmit();
-                System.out.println("Cheese adding form submitted...");
-            }
+        // TODO test :)
+        Form form = new Form("form", new CompoundPropertyModel(cheese));
 
-            @Override
-            protected void onError() {
-                super.onError();
-                System.out.println("Error when adding a cheese...");
-            }*/
-        };
         add(form);
 
-        form.add(new TextField("name",
-                new PropertyModel(cheese, "name")));
-        form.add(new TextField("description",
-                new PropertyModel(cheese, "description")));
-        form.add(new TextField("price",
-                new PropertyModel(cheese, "price")));
+        form.add(new TextField("name"));
+        form.add(new TextField("description"));
+        form.add(new TextField("price"));
+
         form.add(new Button("add"){
             @Override
             public void onSubmit() {
                 System.out.println("Add Cheese clicked;");
-                getCheeseSession().getDataCache().addCheese(cheese);
+                getCheeseSession().getDataCache().addCheese((Cheese)getParent().getModelObject());
                 setResponsePage(ViewCheeses.class);
             }
         });
